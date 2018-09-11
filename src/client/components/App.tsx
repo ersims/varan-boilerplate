570b0d928b2737b2780aea4811803bab10cc6014
ersims <ersims88@gmail.com>
2018-09-11 22:51:29 +0200

    chore(deps): update dependencies

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -20,7 +20,6 @@ class App extends React.Component {
             Varan
           </title>
           <meta charSet="utf-8" />
-          <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="A varan react starter app" />
